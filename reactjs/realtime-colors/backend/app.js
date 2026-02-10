9f6b6576d21b06047649eea30d6a33f04e4ca681
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-10 23:14:52 +0300

    update

--- reactjs/realtime-colors/backend/app.js
+++ reactjs/realtime-colors/backend/app.js
@@ -6,29 +6,29 @@ const cors = require("cors");
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send("hello");
+  res.send("hello");
 });
 
 let lastColor = "#282c34";
 
 io.on("connection", (socket) => {
-	console.log("bir kullanıcı bağlandı!");
+  console.log("bir kullanıcı bağlandı!");
 
-	socket.emit("receive", lastColor);
+  socket.emit("receive", lastColor);
 
-	socket.on("newColor", (color) => {
-		console.log(color);
+  socket.on("newColor", (color) => {
+    console.log(color);
 
-		lastColor = color;
-		io.emit("receive", color);
-		//socket.broadcast.emit("receive", color);
-		//renk seçildiği anda renk gönderen kişi dışındakilere gönderilir
-		//bağlı olan client harici diğer tüm clientlara bu veriyi ilet
-	});
+    lastColor = color;
+    io.emit("receive", color);
+    //socket.broadcast.emit("receive", color);
+    //renk seçildiği anda renk gönderen kişi dışındakilere gönderilir
+    //bağlı olan client harici diğer tüm clientlara bu veriyi ilet
+  });
 
-	socket.on("disconnect", () => {
-		console.log("Bir kullanıcı ayrıldı.");
-	});
+  socket.on("disconnect", () => {
+    console.log("Bir kullanıcı ayrıldı.");
+  });
 });
 
 http.listen(3001, () => console.log("Server is up 🚀 🚀"));
