c7225642db48172c4e8a5c2bf575b2ab0a60caed
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-11 20:32:29 +0300

    setup apollo client

--- graphql-101/event-manager/client/src/main.jsx
+++ graphql-101/event-manager/client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
 import "./index.css";
 import App from "./App.jsx";
+import client from "./apollo";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 );
