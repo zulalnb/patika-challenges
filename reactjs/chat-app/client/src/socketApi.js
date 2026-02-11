4130e79c3c741a7955b786167809e1a85b6f5c38
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-11 14:16:27 +0300

    update chat app

--- reactjs/chat-app/client/src/socketApi.js
+++ reactjs/chat-app/client/src/socketApi.js
@@ -3,33 +3,33 @@ import io from "socket.io-client";
 let socket;
 
 export const init = () => {
-    console.log("Connecting...");
+  console.log("Connecting...");
 
-    socket = io("http://localhost:3000", {
-        transports: ["websocket"],
-    })
+  socket = io("http://localhost:3000", {
+    transports: ["websocket"],
+  });
 
-    socket.on("connect", () => console.log("Connected!"))
+  socket.on("connect", () => console.log("Connected!"));
 };
 
 export const sendMessage = (message) => {
-    if (socket) socket.emit("new-message", message);
+  if (socket) socket.emit("new-message", message);
 };
 
 export const subscribeChat = (cb) => {
-    if (!socket) return;
+  if (!socket) return;
 
-    socket.on("receive-message", (message) => {
-        console.log("yeni mesaj var", message);
-        cb(message);
-    });
+  socket.on("receive-message", (message) => {
+    console.log("yeni mesaj var", message);
+    cb(message);
+  });
 };
 
 export const subscribeInitialMessages = (cb) => {
-    if (!socket) return;
+  if (!socket) return;
 
-    socket.on("message-list", (messages) => {
-        console.log("Initial", messages);
-        cb(messages);
-    });
-}
+  socket.on("message-list", (messages) => {
+    console.log("Initial", messages);
+    cb(messages);
+  });
+};
