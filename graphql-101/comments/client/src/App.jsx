34ee409d0edc3a1fe559a41499c0ae99f1cb57de
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-12 10:29:54 +0300

    install and setup apollo client

--- graphql-101/comments/client/src/App.jsx
+++ graphql-101/comments/client/src/App.jsx
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <div></div>;
+  return <div>hello world</div>;
 }
 
 export default App;
--- graphql-101/comments/client/src/main.jsx
+++ graphql-101/comments/client/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 );
