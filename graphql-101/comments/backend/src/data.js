eabcb9639c0e40dbd723f40f6490748dcb4628e7
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-12 09:31:42 +0300

    add desc to post and profile photo to user

--- graphql-101/comments/backend/src/data.js
+++ graphql-101/comments/backend/src/data.js
@@ -2,11 +2,13 @@ const users = [
   {
     id: "1",
     fullName: "Mehmet Seven",
+    profile_photo: "https://randomuser.me/api/portraits/men/32.jpg",
     age: 32,
   },
   {
     id: "2",
     fullName: "Ahmet Günal",
+    profile_photo: "https://randomuser.me/api/portraits/men/62.jpg",
     age: 34,
   },
 ];
@@ -15,16 +17,22 @@ const posts = [
   {
     id: "1",
     title: "Mehmet'in gönderisi",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit provident eum in aut doloremque saepe nulla recusandae expedita accusamus sint. Minus similique dolorem harum est esse aliquam cumque ipsam accusamus!",
     user_id: "1",
   },
   {
     id: "2",
     title: "Mehmet'in diğer gönderisi",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
     user_id: "1",
   },
   {
     id: "3",
     title: "Ahmet'in gönderisi",
+    description:
+      "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.",
     user_id: "2",
   },
 ];
