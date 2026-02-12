7e0c094123a773c8d3b69df39385402145ce1458
Zülal Nebin <zllnbn1997@gmail.com>
2026-02-12 11:09:23 +0300

    build ui skeleton

--- graphql-101/comments/client/src/main.jsx
+++ graphql-101/comments/client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App";
 
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo";
